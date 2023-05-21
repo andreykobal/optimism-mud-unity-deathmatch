e5a752db9efb1205fbb309a600a2b3fef79f9830
Andreas Kobal <kobalandrey@gmail.com>
2023-05-22 00:59:13 +0100

    Update LeaderboardCounterSystem.sol

--- packages/contracts/src/systems/LeaderboardCounterSystem.sol
+++ packages/contracts/src/systems/LeaderboardCounterSystem.sol
@@ -1,4 +1,3 @@
-// SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0;
 
 import { System } from "@latticexyz/world/src/System.sol";
@@ -7,12 +6,14 @@ import { Counter } from "../codegen/Tables.sol";
 
 bytes32 constant KillsKey = bytes32(uint256(0x060D));
 bytes32 constant DeathsKey = bytes32(uint256(0x060E));
+bytes32 constant ScoreKey = bytes32(uint256(0x060F));
 
 contract LeaderboardCounterSystem is System {
     struct Player {
         string name;
         uint32 kills;
         uint32 deaths;
+        uint32 score;
     }
 
     mapping(address => Player) private players;
@@ -35,6 +36,17 @@ contract LeaderboardCounterSystem is System {
         return newValue;
     }
 
+    function incrementScore(uint32 value) public returns (uint32) {
+        bytes32 key = ScoreKey;
+        uint32 counter = Counter.get(key);
+        uint32 newValue = counter + (value * 100); // Multiply the value by 100
+        Counter.set(key, newValue);
+        updatePlayerScore(newValue);
+        return newValue;
+    }
+
+
+
     function updatePlayerKills(uint32 newValue) private {
         address playerAddress = msg.sender;
         Player storage player = players[playerAddress];
@@ -47,14 +59,29 @@ contract LeaderboardCounterSystem is System {
         player.deaths = newValue;
     }
 
+    function updatePlayerScore(uint32 newValue) private {
+        address playerAddress = msg.sender;
+        Player storage player = players[playerAddress];
+        player.score = newValue / 100; // Divide the score by 100 to get the actual value
+    }
+
     function updatePlayerName(string memory newName) public {
         address playerAddress = msg.sender;
         Player storage player = players[playerAddress];
         player.name = newName;
+        //TO DO Get photon nick name
     }
 
-    function getPlayerStats(address playerAddress) public view returns (string memory, uint32, uint32) {
+    function getPlayerStats(address playerAddress) public view returns (string memory, uint32, uint32, uint32) {
         Player storage player = players[playerAddress];
-        return (player.name, player.kills, player.deaths);
+        return (player.name, player.kills, player.deaths, player.score);
+    }
+
+    function getGlobalLeaderboard() public view returns (address[] memory, uint32[] memory) {
+        uint32[] memory scores = new uint32[](1);
+        scores[0] = Counter.get(ScoreKey);
+        address[] memory addresses = new address[](1);
+        addresses[0] = msg.sender;
+        return (addresses, scores);
     }
 }
